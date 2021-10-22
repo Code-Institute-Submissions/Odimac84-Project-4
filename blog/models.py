8e63c64d16167a0f0964ac567ed6d95242d020f8
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-10-22 18:31:28 +0000

    add template with connections to adding new content for user

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
+    title_tag = models.CharField(max_length=150, default="Read My Mind")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+
     
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -7,6 +7,13 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class PostView(DetailView):
     model = Post
     template_name = 'blogpost.html'
+
+
+class Add_BlogEntry(CreateView):
+    model = Post
+    template_name = 'add-blogentry.html'
+    fields = '__all__'
