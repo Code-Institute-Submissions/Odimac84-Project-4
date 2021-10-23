256a45a23dc7e95d9c7a43a75120f961fe47d710
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-10-23 12:12:53 +0000

    Add summernote to blog entry form

--- Project4/urls.py
+++ Project4/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('members/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
