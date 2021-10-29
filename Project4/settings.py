714d8b89dcffbabf9cfe9622a2501954c77c7165
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-10-29 22:11:22 +0000

    fix redirect for registerpage

--- Project4/settings.py
+++ Project4/settings.py
@@ -63,6 +63,7 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+SIGNUP_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -160,4 +161,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
